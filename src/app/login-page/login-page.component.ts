4de83b561cd1f7718c02b151326a0ad4cc808df6
Khoi Phan <khoiphan21@gmail.com>
2016-12-31 13:10:16 +1000

    finished wiring up the code for the mock login

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  private username: string;
+  private password: string;
+  private error: boolean;
+
+  login() {
+    if (this.username === "kerko" && this.password === "kerkofood") {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.error = true;
+    }
+  }
+
+  constructor(private router: Router) {
+   }
 
   ngOnInit() {
   }
